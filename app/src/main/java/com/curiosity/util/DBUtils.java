69228e04650470d13a2ca0f946648e4f311789ea
curiosity-hyf <1184581135@qq.com>
2016-09-17 13:29:39 +0800

    add something to do

---
+++ app/src/main/java/com/curiosity/util/DBUtils.java
@@ -0,0 +1,8 @@
+package com.curiosity.util;
+
+/**
+ * Created by Curiosity on 2016-9-17.
+ */
+public class DBUtils {
+    // TODO: 添加数据库工具类
+}
---
+++ app/src/main/java/com/curiosity/util/OkHttpUtils.java
@@ -0,0 +1,8 @@
+package com.curiosity.util;
+
+/**
+ * Created by Curiosity on 2016-9-17.
+ */
+public class OkHttpUtils {
+    //TODO: 添加OkHttp 包装类
+}
